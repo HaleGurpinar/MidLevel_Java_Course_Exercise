becfeffc5d2f0dc02c4b9b109f03559bc97a0ea6
HaleGurpinar <106753721+HaleGurpinar@users.noreply.github.com>
2023-05-03 16:36:18 +0300

    Ex

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,35 @@
 package com.company;
 
-
-import generics.Method;
-
 public class Main {
 
     public static void main(String[] args) {
 
+        String str= "Radar", str2="xox", str3="Hi";
+        System.out.println(isPalindrome(str));
+        System.out.println(isPalindrome(str2));
+        System.out.println(isPalindrome(str3));
+
 
     }
 
+    public static String isPalindrome(String str){
+        String reverseStr = "";
+        String result="";
+
+        //int strLength = str.length();
+
+        for (int i = str.length() - 1; i >=0; --i) {
+            reverseStr = reverseStr + str.charAt(i);
+        }
+
+        if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+             result="Yes";
+        }
+        else {
+            result= "No";
+        }
+    return result;}
 }
+
+
+
